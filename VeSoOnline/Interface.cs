4c3e038fd400b9cddf15614064f339efdfb7a24b
John Whisker <nguyen.dk0952@sinhvien.hoasen.edu.vn>
2017-10-17 13:54:11 +0700

    Minor

--- VeSoOnline/Interface.cs
+++ VeSoOnline/Interface.cs
@@ -27,12 +27,13 @@ namespace VeSoOnline
         public static void PrintAllTickets() {
             Console.Clear();
             Database.GetAllTickets();
+            //Console.WriteLine("Không có dãy số này đang được mở bán.");
             Console.WriteLine("{0,-10} | {1,-10} | {2,-10} | {3,10}", "Dãy số", "Ngày xổ", "Số người mua", "Giá tiền");
             Console.WriteLine("----------------------------------------------------");
             Console.WriteLine("{0,-10} | {1,-10} | {2,-12} | {3,10}", "123456", "11-10-2017", "20", "10,000VNĐ");
             Console.WriteLine("{0,-10} | {1,-10} | {2,-12} | {3,10}", "654321", "10-10-2017", "113", "10,000VNĐ");
             IO.PromptOption("trở về màn hình chính", -2);
-
+            
         }
         public static void PrintAllBroughtTickets(Users userInfo) {
             Console.Clear();
